a69d30aed88c29c14c4f1b67c0b8c567cb14a555
Seyma Akdogan <86536953+SeymaAkdogan@users.noreply.github.com>
2022-10-14 13:35:55 +0300

    Update settings.py

--- home_rental_app/settings.py
+++ home_rental_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--569)nn(fz52h^gcbb8y@as4q__7fthq0gpmb60gt%jy8&1*k&'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
